e29acae45976eb16832fbf3ea1a6ce9768d77727
YannGuidon <github@f-cpu.org>
2026-03-27 21:11:15 +0100

    Update project.v

--- src/project.v
+++ src/project.v
@@ -62,17 +62,17 @@ module tt_um_ygdes_hdsiso8_dlhq (
   // multiplexed output
   // assign uio_out = SHOW_LFSR ? LFSR_state8 : Decoded8 ;
   wire SHOW_LFSR_n1, SHOW_LFSR_n2;
-  (* keep *) sg13g2_inv_4 negShow1(.Y(SHOW_LFSR_n1), .A(SHOW_LFSR));
-  (* keep *) sg13g2_inv_4 negShow2(.Y(SHOW_LFSR_n2), .A(SHOW_LFSR));
+  (* keep *) sg13cmos5l_inv_4 negShow1(.Y(SHOW_LFSR_n1), .A(SHOW_LFSR));
+  (* keep *) sg13cmos5l_inv_4 negShow2(.Y(SHOW_LFSR_n2), .A(SHOW_LFSR));
   wire [7:0] LFSR_state8, Decoded8;
-  (* keep *) sg13g2_mux2_2 mux_uio0(.A0(LFSR_state8[0]), .A1(Decoded8[0]), .S(SHOW_LFSR_n1), .X(uio_out[0]));
-  (* keep *) sg13g2_mux2_2 mux_uio1(.A0(LFSR_state8[1]), .A1(Decoded8[1]), .S(SHOW_LFSR_n1), .X(uio_out[1]));
-  (* keep *) sg13g2_mux2_2 mux_uio2(.A0(LFSR_state8[2]), .A1(Decoded8[2]), .S(SHOW_LFSR_n1), .X(uio_out[2]));
-  (* keep *) sg13g2_mux2_2 mux_uio3(.A0(LFSR_state8[3]), .A1(Decoded8[3]), .S(SHOW_LFSR_n1), .X(uio_out[3]));
-  (* keep *) sg13g2_mux2_2 mux_uio4(.A0(LFSR_state8[4]), .A1(Decoded8[4]), .S(SHOW_LFSR_n2), .X(uio_out[4]));
-  (* keep *) sg13g2_mux2_2 mux_uio5(.A0(LFSR_state8[5]), .A1(Decoded8[5]), .S(SHOW_LFSR_n2), .X(uio_out[5]));
-  (* keep *) sg13g2_mux2_2 mux_uio6(.A0(LFSR_state8[6]), .A1(Decoded8[6]), .S(SHOW_LFSR_n2), .X(uio_out[6]));
-  (* keep *) sg13g2_mux2_2 mux_uio7(.A0(LFSR_state8[7]), .A1(Decoded8[7]), .S(SHOW_LFSR_n2), .X(uio_out[7]));
+  (* keep *) sg13cmos5l_mux2_2 mux_uio0(.A0(LFSR_state8[0]), .A1(Decoded8[0]), .S(SHOW_LFSR_n1), .X(uio_out[0]));
+  (* keep *) sg13cmos5l_mux2_2 mux_uio1(.A0(LFSR_state8[1]), .A1(Decoded8[1]), .S(SHOW_LFSR_n1), .X(uio_out[1]));
+  (* keep *) sg13cmos5l_mux2_2 mux_uio2(.A0(LFSR_state8[2]), .A1(Decoded8[2]), .S(SHOW_LFSR_n1), .X(uio_out[2]));
+  (* keep *) sg13cmos5l_mux2_2 mux_uio3(.A0(LFSR_state8[3]), .A1(Decoded8[3]), .S(SHOW_LFSR_n1), .X(uio_out[3]));
+  (* keep *) sg13cmos5l_mux2_2 mux_uio4(.A0(LFSR_state8[4]), .A1(Decoded8[4]), .S(SHOW_LFSR_n2), .X(uio_out[4]));
+  (* keep *) sg13cmos5l_mux2_2 mux_uio5(.A0(LFSR_state8[5]), .A1(Decoded8[5]), .S(SHOW_LFSR_n2), .X(uio_out[5]));
+  (* keep *) sg13cmos5l_mux2_2 mux_uio6(.A0(LFSR_state8[6]), .A1(Decoded8[6]), .S(SHOW_LFSR_n2), .X(uio_out[6]));
+  (* keep *) sg13cmos5l_mux2_2 mux_uio7(.A0(LFSR_state8[7]), .A1(Decoded8[7]), .S(SHOW_LFSR_n2), .X(uio_out[7]));
 
 
 ////////////////////////////// custom soup //////////////////////////////
@@ -80,19 +80,19 @@ module tt_um_ygdes_hdsiso8_dlhq (
   // select the clock
   // CLK_OUT = clk if CLK_SEL=0, else EXT_CLK
   // assign CLK_OUT = CLK_SEL ? EXT_CLK : clk;
-  (* keep *) sg13g2_mux2_2 mux_clk(.A0(clk), .A1(EXT_CLK), .S(CLK_SEL), .X(CLK_OUT));
+  (* keep *) sg13cmos5l_mux2_2 mux_clk(.A0(clk), .A1(EXT_CLK), .S(CLK_SEL), .X(CLK_OUT));
   // ring oscillator anyone ?
-  (* keep *) sg13g2_inv_4 negClkOut(.Y(CLK_OUTn), .A(CLK_OUT));
+  (* keep *) sg13cmos5l_inv_4 negClkOut(.Y(CLK_OUTn), .A(CLK_OUT));
 
   wire INT_RESET;
   // Combined and resynch'ed Reset
-  (* keep *) sg13g2_dfrbpq_2 DFF_reset(.Q(INT_RESET), .D(EXT_RST), .RESET_B(rst_n), .CLK(CLK_OUT));
+  (* keep *) sg13cmos5l_dfrbpq_2 DFF_reset(.Q(INT_RESET), .D(EXT_RST), .RESET_B(rst_n), .CLK(CLK_OUT));
 
 
   // Select + resynch D_in
   wire SISO_in;
   //      SISO_in <= DIN_SEL ? LFSR_BIT : D_IN;
-  (* keep *) sg13g2_sdfrbpq_1 sync_Din(.Q(SISO_in), .D(D_IN),
+  (* keep *) sg13cmos5l_sdfrbpq_1 sync_Din(.Q(SISO_in), .D(D_IN),
                          .SCD(LFSR_BIT), .SCE(DIN_SEL), .RESET_B(INT_RESET), .CLK(CLK_OUT));
 
 ////////////////////////////// sub-modules //////////////////////////////
@@ -110,21 +110,6 @@ module tt_um_ygdes_hdsiso8_dlhq (
     .RESET(INT_RESET),
     .DFF4(Johnson4),
     .Decoded8(Decoded8));
-
-/* version : direct loopback, 20 cycles
-  wire [3:0] siso_start_even, siso_start_odd;
-
-  siso_demux_mux_dl demux_mux(
-    .RESET(INT_RESET),
-    .CLK(CLK_OUT),
-    .Din(SISO_in),
-    .Latch8(Decoded8),
-    .siso_first_even(siso_start_even),
-    .siso_first_odd(siso_start_odd),
-    .siso_last_even(siso_start_even),
-    .siso_last_odd(siso_start_odd),
-    .Dout(D_OUT));
-*/
 
 /* longer version, 384+96+22=502 cycles,
     about 9 cycles in "advance" of the LFSR period pulse */
